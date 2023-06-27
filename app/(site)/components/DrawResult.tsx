4b469e1906794ff22c15bb2bbc8ee7875ba37273
KAL <kevin12147@yahoo.com.tw>
2023-06-27 12:02:41 +0800

    better syntax for Object.entires

--- app/(site)/components/DrawResult.tsx
+++ app/(site)/components/DrawResult.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { useRef, useState } from "react";
+import React from "react";
+import { useRef } from "react";
 
 interface DrawResultProps {
   setModal: any;
@@ -29,19 +29,18 @@ export default function DrawResult(props: DrawResultProps) {
         <div className="flex justify-center items-center w-full h-auto bg-white border-solid border-2 border-black p-5 mx-2 sm:w-[380px] rounded-md">
           {display.map((value: string, index: number) => (
             <div key={index}>
-              {Object.entries(value).map((item) => (
+              {Object.entries(value).map(([key, value]) => (
                 <div
-                  key={item[0]}
+                  key={key}
                   className="flex flex-col justify-center items-center text-xl"
                   style={{
                     color:
-                      compare.filter((obj: any) => obj[0] === item[0])[0][2] <=
-                      20
+                      compare.filter((obj: any) => obj[0] === key)[0][2] <= 20
                         ? "red"
                         : "black",
                   }}
                 >
-                  {item[0]}x{item[1]}
+                  {key}x{value}
                 </div>
               ))}
             </div>
